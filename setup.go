d39839509dd0c37e92ae68fda52783416cfb9461
ThePiachu <ThePiachu@gmail.com>
2016-02-17 16:19:24 -0800

    Fixed the fee, go fmt.

--- setup.go
+++ setup.go
@@ -41,11 +41,11 @@ func (r Setup) Execute(state IState, args []string) error {
 	// Make sure we have a seed.
 	if len(args) != 2 {
 		msg := "You must supply some random seed. For example (don't use this!)\n" +
-		"factom-cli setup 'woe!#in31!%234ng)%^&$%oeg%^&*^jp45694a;gmr@#t4 q34y'\n" +
-		"would make a nice seed.  The more random the better.\n\n" +
-		"Note that if you create an address before you call Setup, you must\n" +
-		"use those address(s) as you access the fountians."
-		
+			"factom-cli setup 'woe!#in31!%234ng)%^&$%oeg%^&*^jp45694a;gmr@#t4 q34y'\n" +
+			"would make a nice seed.  The more random the better.\n\n" +
+			"Note that if you create an address before you call Setup, you must\n" +
+			"use those address(s) as you access the fountians."
+
 		return fmt.Errorf(msg)
 	}
 	setFountian := false
@@ -54,17 +54,17 @@ func (r Setup) Execute(state IState, args []string) error {
 		setFountian = true
 		for i := 1; i <= 10; i++ {
 			name := fmt.Sprintf("%02d-Fountain", i)
-			err := GenAddress(state,"fct",name)
+			err := GenAddress(state, "fct", name)
 			if err != nil {
 				fmt.Println(err)
 				return nil
 			}
 		}
 	}
-	
+
 	seedprime := fct.Sha([]byte(fmt.Sprintf("%s%v", args[1], time.Now().UnixNano()))).Bytes()
 	NewSeed(state, seedprime)
-	
+
 	if setFountian {
 		fmt.Println("New seed set, fountain addresses defined")
 	} else {
@@ -78,10 +78,10 @@ func (r Setup) Name() string {
 }
 
 func (Setup) ShortHelp() string {
-	return "setup <seed>                -- Sets up Fountain addresses, and seeds the wallet\n"+
-	       "                               This only works if no addresses have been created.\n"+
-		   "                               If addresses exist, no Fountain addresses are created.\n"+
-		   "                               But the wallet gets a new seed.\n"
+	return "setup <seed>                -- Sets up Fountain addresses, and seeds the wallet\n" +
+		"                               This only works if no addresses have been created.\n" +
+		"                               If addresses exist, no Fountain addresses are created.\n" +
+		"                               But the wallet gets a new seed.\n"
 }
 
 func (Setup) LongHelp() string {
@@ -96,4 +96,3 @@ Setup <seed>                        Sets up Fountain addresses, and seeds the wa
 func NewSeed(state IState, data []byte) {
 	state.GetFS().GetWallet().NewSeed(data)
 }
-
